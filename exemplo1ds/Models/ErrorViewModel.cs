809503c08838856a5258b7535b50e751d0a60bda
2DS <2ds@bglab.local>
2025-05-23 10:43:40 -0300

    Criando o projeto

---
+++ exemplo1ds/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace exemplo1ds.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ exemplo1ds/Models/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace exemplo1ds.Models
+{
+    public class Pessoa
+    {
+       public int PessoaId { get; set; } // get vai servir para fazer consultas e retorna o valor atual de um atributo e set permite definir ou modificar os valores de um atributo (get pega e set determina) metodos 
+       public string? Nome { get; set; } 
+       public string? Email { get; set; }
+    }
+}
